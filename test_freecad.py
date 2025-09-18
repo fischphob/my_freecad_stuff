8affe17875a004b9a8d0df076ea16f8c91ff5ab8
micha <micha@Fixa.fritz.box>
2025-09-18 22:09:59 +0200

    Initial commit of my first freecad python script

---
+++ test_freecad.py
@@ -0,0 +1,16 @@
+
+# execute in console with
+#   
+
+import FreeCAD
+
+doc = FreeCAD.ActiveDocument
+if doc is None:
+    print("No active document.")
+else:
+    for obj in doc.Objects:
+        if obj.TypeId.lower().startswith('part::'):
+            length = getattr(obj, 'Length', None)
+            height = getattr(obj, 'Height', None)
+            print(f"{obj.Label},{obj.Name},{obj.TypeId},{length},{height}")
+            #print(f"Label: {obj.Label:<30} Name: {obj.Name:<20} Type: {obj.TypeId}")
